4cd1602245070d4e15b21fdf83b7daed88db7d54
loren <lorenappff@gmail.com>
2025-09-02 11:19:21 +0200

    Exercise 9 finished and corrected

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(from, to) {
+    if (from < 0 || to < 0) {
+        return "ERROR";
+    }
+    else if (typeof from !== "number" || typeof to !== "number" || !Number.isInteger(from) || !Number.isInteger(to)) {
+        return "ERROR";
+    }
+    else {
+        let sum = 0;
+        if (from>to) {
+            for (let i=from; i>=to; i--) {
+            sum+=i;
+        }
+        } else {
+            for (let i=from; i<=to; i++) {
+            sum+=i;
+            }
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
